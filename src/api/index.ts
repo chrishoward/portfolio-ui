ac96cd0bf2b906bbf90c5ee12df64227629c7a33
Chris Howard <chrisau233@gmail.com>
2021-07-12 11:25:39 +1000

    Use docker container for testing

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,8 @@
 const host = process.env.REACT_APP_API_HOST;
 const port = process.env.REACT_APP_API_PORT;
-const prod = process.env.NODE_ENV === "production";
-const protocol = `http${prod ? "s" : ""}`;
+const usingProdApi =
+  process.env.NODE_ENV === "production" || process.env.NODE_ENV === "test";
+const protocol = `http${usingProdApi ? "s" : ""}`;
 const apiUrl = `${protocol}://${host}:${port}/`;
 
 export async function getPortfolioData() {
